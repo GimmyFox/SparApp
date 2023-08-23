483bd3e811c2e7edf8d5f024d1777ebea197974f
GimmyFox <mguzeev99@gmail.com>
2023-08-23 12:43:39 +0500

    Initial Commit

---
+++ SparApp/SparAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparAppApp.swift
+//  SparApp
+//
+//  Created by Maksim Guzeev on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SparAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
